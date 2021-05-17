571ed1c87533a58d7b034444cea748cf1e6ae028
diviner <qaz60908@gmail.com>
2021-05-17 17:14:38 +0800

    Commit for finished parser eml to list , and data is json format

--- decode.py
+++ decode.py
@@ -1,74 +1,89 @@
 from flanker import mime
 from bs4 import BeautifulSoup
-import json , re
+from os import listdir
+import csv
 
-raw_email = "./01.eml"
+def eml_to_list(my_eml):
+    with open(my_eml, 'rb') as fhdl:
+        raw_email = fhdl.read()
+    msg = mime.from_string(raw_email)
+    for part in msg.parts:
+        container = []
+        container2 = []
+        if(part.content_type == "text/html"):
+            soup = BeautifulSoup(part.body, "html.parser")
+            res = soup.table.table
+            x = res.find_all("tr")
+            print(type(x))
+            for item in x:
+                q = item.text.replace(" ","").replace('\u3000',"").replace('▍',"").splitlines()
+                container.append(q)
+            v = list(filter(lambda x: x, container))
+            v.pop()
+            resumes = []
+            split_resumes = []
+            coll = []
+            for i in v:
+                container2.append(i[0])
+            container2.pop(0)
+            container2.pop(0)
+            container2.pop()
+            result = ','.join(container2)
+            info = result.split("最後修改")
+            for s in info:
+                if(len(s) < 100):
+                    info.remove(s)
+            for i in info:
+                resumes.append(i.split("專長")[0])
+            for x in resumes:
+                record = x.split(",")
+                record.pop(0)
+                record.pop(0)
+                split_resumes.append(record)
+            mytest = list(filter(lambda y: y ,split_resumes))
+            for sub_list in mytest:
+                d = {}
+                if(len(sub_list[-1]) <= 5):
+                    sub_list.pop()
+                for count , record in enumerate(sub_list):
+                    if "代碼" in record:
+                        d["姓名"] = record.split("代碼")[0]
+                    if("男" in record) or ("女" in record):
+                        d["性別"] = record.split("|")[0]
+                        d["年齡"] = record.split("|")[1]
+                    if "聯絡電話" in record:
+                        d["聯絡電話"] = record.split("聯絡電話")[1]
+                    if "電子郵件" in record:
+                        d["電子郵件"] = record.split("電子郵件")[1]
+                    if "聯絡地址" in record:
+                        d["聯絡地址"] = record.split("聯絡地址")[1]
+                    if "教育程度" in record:
+                        d["教育程度"] = record.split("教育程度")[1]
+                    if "職務類別" in record:
+                        d["求職類別"] = record.split("職務類別")[1]
+                    if "工作經驗累計年資" in record:
+                        d["累計年資"] = record.split("工作經驗累計年資")[1]
+                    if "累計經驗" in record:
+                        d["累計經驗"] = record.split("累計經驗")[1]
+                        d["過往公司"] = ",".join(sub_list[count+1:])
+                coll.append(d)
+            return coll
 
-with open(raw_email, 'rb') as fhdl:
-    raw_email = fhdl.read()
+# 開啟即將寫入的 csv
+with open('output.csv', 'w', newline='') as csvfile:
+  # 定義欄位
+  fieldnames = ['姓名', '性別', '年齡','聯絡電話','教育程度','職務類別','工作經驗累計年資','累計經驗','過往公司']
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
 
-msg = mime.from_string(raw_email)
-msg_item = msg.headers.items()
-demo = json.dumps(msg_item)
-msg_parts = len(msg.parts)
-msg_body = msg.body
-for part in msg.parts:
-    container = []
-    container2 = []
-    search_jobs = []
-    sex_list = []
-    if(part.content_type == "text/html"):
-        #print(part.body)
-        #list1 = part.body.split()
-        #print(str(list1))
-        soup = BeautifulSoup(part.body, "html.parser")
-        # text_file = open("output3.html", "w")
-        # text_file.write(soup.prettify())
-        # text_file.close()
-        result = soup.select('[style*="font-size:24px"]')
-        #for item in result:
-            #print(item.text)
-        result2 = soup.select('b[style*="font-size:15px"]')
-        res = soup.table.table
-        x = res.find_all("tr")
-        print(type(x))
-        for item in x:
-            q = item.text.replace(" ","").replace('\u3000',"").replace('▍',"").splitlines()
-            container.append(q)
-        v = list(filter(lambda x: x, container))
-        #print(v)
-        v.pop()
-        for i in v:
-            container2.append(i[0])        
-        container2.pop(0)
-        container2.pop(0)
-        container2.pop()
-        for p in container2:
-            if "求職條件職務類別" in p:
-                search_jobs.append(p)
-            if("女" in p) | ("男" in p):
-                sex_list.append(p)
-        #print(sex_list)
-        #print(search_jobs)
-        # v.pop(0)
-        # v.pop(0)
-        # v.pop()
-        # v.pop()
-        tfile = open("list2.txt","w")
-        tfile.write(str(container2))
-        tfile.close()
-        
-        #for item in result2:
-            #print(item.text)
-        # test = soup.find_all("table")
-        #print(test[0].find_all('b'))
-        # for item in test:
-        #     x = item.find_all('table')
-        #     for i in x:
-        #         z = i.find_all('table')
-        #         print(len(z))
-                # for t in z:
-                #     u = t.find_all("b")
-                #     res = t.text.replace('\n','').split(" ")
-                #     while "" in res:
-                #         res.remove("")
+# eml 擋存放目錄
+mypath = "emls"
+
+# 取的目錄下所有 eml 擋存入 list
+files = listdir(mypath)
+
+# 讀擋並處理轉換 csv
+for file in files:
+    result = eml_to_list(file)
+    for i in result:
+        writer.writerow(i)
